569d623a94258ca59d91203f3372a9d28482764c
태경 <63376555+taegung@users.noreply.github.com>
2022-03-27 14:31:07 +0900

    Add files via upload

---
+++ ch4 1번.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======================= RESTART: C:/CookPython/ch4 1번.py =======================
+연도를 입력하시오:
